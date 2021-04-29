66f9a3ca9b76099cded360f77ea8c65738178e40
Jhonata Caio <jhonatacaiob@gmail.com>
2021-04-29 02:26:21 -0300

    Aula 04 concluida

--- Secao-4/gradient_discent.py
+++ Secao-4/gradient_discent.py
@@ -4,20 +4,41 @@ import numpy as np
 def f(x):
     return x**2 + x + 1
 
-
+def df(x):
+    return 2*x + 1
 
 
 x_1 = np.linspace(start = -3, stop = 3, num = 500)
 
 
+plt.figure(figsize=[15,5])
 
+
+plt.subplot(1,2,1)
+plt.title("Função de custo")
 plt.xlim(-3, 3)
-plt.ylim(min(f(x_1)), max(f(x_1)))
+plt.ylim(0, 8)
 
 plt.xlabel("x")
 plt.ylabel("f(x)")
 
-plt.plot(x_1, f(x_1))
-plt.savefig("Aula 03.jpg")
+plt.plot(x_1, f(x_1), color = "blue", linewidth = 5)
 
 
+
+
+
+plt.subplot(1,2,2)
+plt.title("Derivada da funçao de custo")
+plt.xlim(-2, 3)
+plt.ylim(-3, 6)
+plt.grid("True")
+
+plt.xlabel("x")
+plt.ylabel("df(x)")
+
+plt.plot(x_1, df(x_1), color = "skyblue", linewidth = 5)
+
+
+
+plt.savefig("Aula 04.jpg")
